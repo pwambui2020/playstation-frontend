290621917010e161f381a47e5ee8a36e5f9011df
RonaldKiprotich <protich12@gmail.com>
2021-01-13 15:56:11 +0300

    login endpoint

--- js/login.script.js
+++ js/login.script.js
@@ -5,7 +5,7 @@ form.addEventListener('submit',function(e){
     var username = document.getElementById('username1').value    
     var password= document.getElementById('password1').value
 
-fetch('http://127.0.0.1:8000/api/login/', {
+fetch('https://finessent12.herokuapp.com/api/login/', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
